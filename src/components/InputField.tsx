1be6da3fe1e615cb4634b559f8a3e63dc3340221
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2023-02-23 17:53:38 +0200

    logic to save all dodos, styling

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './styles.css';
 
 interface Props {
@@ -8,9 +8,17 @@ interface Props {
 }
 
 const InputField = ({ toDo, setToDo, handleAdd }: Props) => {
+    const inputRef = useRef<HTMLInputElement>(null);
     return (
-        <form className='input' onSubmit={handleAdd}>
+        <form
+            className='input'
+            onSubmit={(e) => {
+                handleAdd(e);
+                inputRef.current?.blur();
+            }}
+        >
             <input
+                ref={inputRef}
                 className='input__box'
                 type='input'
                 placeholder='Enter a task'
